5221aa472860a7bd1dde492e26a1c04ea65222cf
zhangpengxiang <zhangpengxiang@hosonsoft.com>
2024-12-12 14:52:14 +0800

    数据库源

---
+++ src/main/resources/static/user.sql
@@ -0,0 +1,42 @@
+create table user
+(
+    id            int auto_increment
+        primary key,
+    username      varchar(20) null,
+    password      varchar(20) null,
+    grant_type    varchar(20) null,
+    client_id     varchar(20) null,
+    client_secret varchar(20) null,
+    client_key    varchar(20) null
+);
+
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (1, 'user1', 'password1', 'password_grant', 'client1', 'secret1', 'key1');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (2, 'user2', 'password2', 'password_grant', 'client2', 'secret2', 'key2');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (3, 'user3', 'password3', 'password_grant', 'client3', 'secret3', 'key3');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (4, 'user4', 'password4', 'password_grant', 'client4', 'secret4', 'key4');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (5, 'user5', 'password5', 'password_grant', 'client5', 'secret5', 'key5');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (6, 'user6', 'password6', 'password_grant', 'client6', 'secret6', 'key6');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (7, 'user7', 'password7', 'password_grant', 'client7', 'secret7', 'key7');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (8, 'user8', 'password8', 'password_grant', 'client8', 'secret8', 'key8');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (9, 'user9', 'password9', 'password_grant', 'client9', 'secret9', 'key9');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (10, 'user10', 'password10', 'password_grant', 'client10', 'secret10', 'key10');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (11, 'user11', 'password11', 'password_grant', 'client11', 'secret11', 'key11');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (12, 'user12', 'password12', 'password_grant', 'client12', 'secret12', 'key12');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (13, 'user13', 'password13', 'password_grant', 'client13', 'secret13', 'key13');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (14, 'user14', 'password14', 'password_grant', 'client14', 'secret14', 'key14');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (15, 'user15', 'password15', 'password_grant', 'client15', 'secret15', 'key15');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (16, 'user16', 'password16', 'password_grant', 'client16', 'secret16', 'key16');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (17, 'user17', 'password17', 'password_grant', 'client17', 'secret17', 'key17');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (18, 'user18', 'password18', 'password_grant', 'client18', 'secret18', 'key18');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (19, 'user19', 'password19', 'password_grant', 'client19', 'secret19', 'key19');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (20, 'user20', 'password20', 'password_grant', 'client20', 'secret20', 'key20');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (21, 'user21', 'password21', 'password_grant', 'client21', 'secret21', 'key21');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (22, 'user22', 'password22', 'password_grant', 'client22', 'secret22', 'key22');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (23, 'user23', 'password23', 'password_grant', 'client23', 'secret23', 'key23');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (24, 'user24', 'password24', 'password_grant', 'client24', 'secret24', 'key24');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (25, 'user25', 'password25', 'password_grant', 'client25', 'secret25', 'key25');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (26, 'user26', 'password26', 'password_grant', 'client26', 'secret26', 'key26');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (27, 'user27', 'password27', 'password_grant', 'client27', 'secret27', 'key27');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (28, 'user28', 'password28', 'password_grant', 'client28', 'secret28', 'key28');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (29, 'user29', 'password29', 'password_grant', 'client29', 'secret29', 'key29');
+INSERT INTO zpx.user (id, username, password, grant_type, client_id, client_secret, client_key) VALUES (30, 'user30', 'password30', 'password_grant', 'client30', 'secret30', 'key30');
